ec5bb5a5a5ca048d365a3d9e300e8aef49b99e3c
DavidPridal <pridal.david@st.spsoa.cz>
2025-10-31 09:13:14 +0100

    2. commit -  vytvorena trida BankovniUcet a objekt

---
+++ src/cz/pridalsps/BankovniUcet.java
@@ -0,0 +1,13 @@
+package cz.pridalsps;
+
+public class BankovniUcet {
+    private final String cisloUctu;
+    private String jmenoMajitele;
+    private double stavUctu;
+
+    public BankovniUcet(String cisloUctu, String jmenoMajitele, double stavUctu) {
+        this.cisloUctu = cisloUctu;
+        this.jmenoMajitele = jmenoMajitele;
+        this.stavUctu = 0;
+    }
+}
--- src/cz/pridalsps/Main.java
+++ src/cz/pridalsps/Main.java
@@ -2,6 +2,8 @@ package cz.pridalsps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projekt na zacatku v1");
+        System.out.println("Projekt se tridou BankovniUcet v2");
+        BankovniUcet bu01 = new BankovniUcet("127-2566771/0100","Jan Novák", 0);
+
     }
 }
